b069d26342e2ac0cd32fbc76b6529d63f45b5ddf
Issam Mezgueldi <issammez44@gmail.com>
2024-03-24 11:59:38 +0000

    add tables to the roles and permissions pages

--- app/iam/permissions/page.tsx
+++ app/iam/permissions/page.tsx
@@ -1,9 +1,14 @@
 import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
+import TableThree from "@/components/Tables/TableThree";
 
 const Permissions = () => {
   return (
     <div className="mx-auto">
       <Breadcrumb pageName="Permissions Managment" />
+
+      <div>
+        <TableThree />
+      </div>
     </div>
   );
 };
--- app/iam/roles/page.tsx
+++ app/iam/roles/page.tsx
@@ -1,9 +1,14 @@
 import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
+import TableThree from "@/components/Tables/TableThree";
 
 const Roles = () => {
   return (
     <div className="mx-auto">
       <Breadcrumb pageName="Roles Managment" />
+
+      <div>
+        <TableThree />
+      </div>
     </div>
   );
 };
